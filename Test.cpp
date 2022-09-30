863ead5cd1d466b5ae647faeae5cae2ac6a4122b
Unsurefeedback <114749725+Unsurefeedback@users.noreply.github.com>
2022-09-30 21:38:29 +0800

    Add a file

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"hhh"<<endl;
+    string name;
+    cout<<"请输入名字： "<<endl;
+    cin>>name;
+    system("pause");
+    return 0;
+}
